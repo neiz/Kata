956149ab13617ef1c5d2fce86f88f7d70b05072b
Neil Allen <neil.denis.allen@gmail.com>
2012-07-05 09:37:04 -0400

    Init commit with fizzbuzz

---
+++ FizzBuzz.js
@@ -0,0 +1,30 @@
+﻿// FizzBuzz Kata 
+/*     
+if i is a multiple of 3 print "Fizz"
+else if i is a multiple of 5 print "Buzz"
+else if i is a multiple of 3 and a multiple of 5 print "FizzBuzz"
+else print i
+*/
+
+
+
+//JavaScript
+
+var maxNumber = 100;
+
+for (var count = 1; count <= maxNumber; count++)
+{
+    if (count % 15 == 0) {
+        console.log("fizzbuzz");
+    }
+    else if (count % 3 == 0) {
+        console.log("fizz");
+    }
+    else if (count % 5 == 0) {
+        console.log("buzz");
+    }
+    else {
+        console.log(count);
+    }
+}
+
