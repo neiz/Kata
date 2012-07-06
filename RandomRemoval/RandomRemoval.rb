5713a40d5d75ce75a48319899c78f0fd1e61be69
Neil Allen <neil.denis.allen@gmail.com>
2012-07-06 15:37:04 -0400

    Ruby implementation of RandomRemoval

---
+++ RandomRemoval/RandomRemoval.rb
@@ -0,0 +1,32 @@
+﻿# Removing a Number from Array 
+#     
+#Begin with an array of n integers in no particular order.  Remove one of these.  Which integer was removed?
+#
+#Instead of sorting, it seemed more efficient for me to get an initial count, 
+#and then a count once an integer was removed.  The difference between the sums
+#leaves the removed integer.  This executed faster as n got large.
+#
+# Wrapped in a function for JSLitmus purposes
+
+def RandomRemoval(n) 
+
+    #Initialize array
+	array=*(1..n)
+
+    #Get Initial Count
+    sum = array.inject(:+)
+
+    #Randomize
+    array.shuffle
+    
+    #Remove a random integer
+    #array.sample >= 1.9
+    array.delete_at(array.choice)
+	
+    #Get new sum
+    newSum = array.inject(:+)
+
+    #Output solution
+    print sum-newSum
+
+end
