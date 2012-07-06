64f730a035633efc38fb8e37d90b49bc2557ef67
Neil Allen <neil.denis.allen@gmail.com>
2012-07-06 10:50:55 -0400

    Ruby implementation of fizzbuzz

---
+++ FizzBuzz/FizzBuzz.rb
@@ -0,0 +1,24 @@
+﻿#FizzBuzz Kata  
+#if i is a multiple of 3 print "fizz"
+#else if i is a multiple of 5 print "buzz"
+#else if i is a multiple of 3 and a multiple of 5 print "fizzbuzz"
+#else print i
+
+def fizzbuzz(maxNumber)
+
+	for count in 1..maxNumber
+		if count % 15 == 0 then
+			print "fizzbuzz"
+		elsif count % 3 == 0 then
+			print "fizz"
+		elsif count % 5 == 0 then
+			print "buzz"
+		else
+			print count
+		end
+		print "\n"
+	end
+end
+
+
+puts fizzbuzz(1000)
