42311f7dc3f05f868ab04e987df81cf0c9b45c97
Neil Allen <neil.denis.allen@gmail.com>
2012-07-09 16:16:31 -0400

    PHP BinarySearch - Iterative
    (Wow, that was easy)

---
+++ BinarySearch/BinarySearch.php
@@ -0,0 +1,17 @@
+<?php
+
+// Binary Search 
+/*     
+Arguments are an array of numbers to sort through, and a value which we want to find.
+Assumes array is pre-sorted before entering function.
+*/
+
+// Wrapped in a function for JSLitmus purposes
+
+// Iterative implementation
+
+function iterative($haystack, $needle) {
+	//Built in
+	return array_search($needle, $haystack);
+}
+?>
