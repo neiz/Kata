7a1896d412ded7a4b5a84102bf6acc7a6ae0039f
Neil Allen <neil.denis.allen@gmail.com>
2012-08-17 16:17:27 -0400

    Corrected some logic issues

--- BinarySearch/BinarySearch.js
+++ BinarySearch/BinarySearch.js
@@ -11,14 +11,14 @@ Assumes array is pre-sorted before entering function.
 var iterative = function(haystack, needle) {
     //Initialize min/max values
     minValue = 0;
-    maxValue = haystack.length - 1;
+    maxValue = haystack.length;
     while (minValue <= maxValue) {
         //Calculate i as midpoint
         i = Math.floor((minValue + maxValue) / 2);
         if (haystack[i] < needle) {
             minValue = i + 1;
         }
-        if (haystack[i] > needle) {
+        else if (haystack[i] > needle) {
             maxValue = i - 1;
         }
         else {
@@ -30,7 +30,7 @@ var iterative = function(haystack, needle) {
     return -1;
 }
 var haystack = [0, 1, 4, 5, 6, 7, 8, 9, 12, 26, 45, 67, 78, 90, 98, 123, 211, 234, 456, 769, 865, 2345, 3215, 14345, 24324];
-var needle = 60;
+var needle = 211;
 iterative(haystack, needle);
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -42,14 +42,20 @@ var recursive = function (needle, low, high, haystack) {
     //Initialize variables
     var mid;
 
-    //Loop through low-bound to high-bound
-    while (low < high) {
-        //Get mid-point
+    if (high < low) {
+        //Failure, not found
+        return null;
+    }
+    else
+    {
         mid = Math.floor((low + high) / 2);
+    }
+    //Loop through low-bound to high-bound
+        //Get mid-point
         if (needle < haystack[mid])
         {
             //Reiterate function with mid=high
-            return recursive(needle, low, mid, haystack);
+            return recursive(needle, low, mid-1, haystack);
         }
         else if (needle > haystack[mid]) {
             //Reiterate function with low being middle integer+1
@@ -60,11 +66,10 @@ var recursive = function (needle, low, high, haystack) {
             //Success
             return mid;
         }
-    }
     //Failure; should not have reached this point
     return null;
 }
 
 var haystack = [0, 1, 4, 5, 6, 7, 8, 9, 12, 26, 45, 67, 78, 90, 98, 123, 211, 234, 456, 769, 865, 2345, 3215, 14345, 24324];
-var needle = 60;
-recursive(haystack, needle);
+var needle = 211;
+recursive(needle, 0, haystack.length, haystack);
