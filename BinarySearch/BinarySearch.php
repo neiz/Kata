38bd0cab58b6a1cdbd22b0a73e2974e00f930a59
Neil Allen <neil.denis.allen@gmail.com>
2012-08-20 12:09:29 -0400

    PHP implementation: recursive binarysearch

--- BinarySearch/BinarySearch.php
+++ BinarySearch/BinarySearch.php
@@ -14,4 +14,34 @@ function iterative($haystack, $needle) {
 	//Built in
 	return array_search($needle, $haystack);
 }
+
+
+// Recursive implementation
+
+function recursive($needle, $haystack, $low, $high) {
+    
+    $mid = 0;
+
+    if ($high < $low) {
+        return null;
+    }
+    else {
+        $mid = (round($low+$high)/2);
+    }
+
+        if ($needle < $haystack[$mid])
+        {
+            return recursive($needle, $haystack, $low, $mid - 1); //Reiterate function with mid=high
+        }
+        elseif ($needle > $haystack[$mid])
+        {
+            return recursive($needle, $haystack, $mid + 1, $high); //Reiterate itself; manipulate low value
+        }
+        else
+        {
+            return $mid; //Success
+        }
+        return null; //Failure
+
+}
 ?>
