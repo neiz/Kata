8d00d037a27bbe9a7b2e5c173ab85509b3274218
nallen <neil.allen@ecs-federal.com>
2012-09-14 15:45:34 -0400

    Helper method to check results

--- String Calculator/JS/spec/Tests.js
+++ String Calculator/JS/spec/Tests.js
@@ -2,5 +2,12 @@
 	it('should eval "" to 0', function() {
     var Add;
     expect(StringCalc.Add("")).toEqual(0);
-});
+	});
+
+
+	function check(exp, result) {
+		it('should eval "' + exp + '" to ' + result, function() {
+			expect(StringCalc.Add(exp)).toEqual(result)
+		});
+	}
 });
