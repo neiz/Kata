a52a5bf92dc7b274bc225b21870edd10d3270964
Neil Allen <neil.denis.allen@gmail.com>
2012-10-02 00:28:26 -0400

    Couple C# Katas
    Binary search (iterative) and randomremoval

---
+++ BinarySearch/BinarySearch.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+// Binary Search 
+/*     
+Arguments are an array of numbers to sort through, and a value which we want to find.
+Assumes array is pre-sorted before entering function.
+*/
+
+//C# Implementation
+
+namespace Kata
+{
+    class BinarySearch
+    {
+        static void Main(string[] args)
+        {
+            int needle = 211;
+            int[] haystack = {0, 1, 4, 5, 6, 7, 8, 9, 12, 26, 45, 67, 78, 90, 98, 123, 211, 234, 456, 769, 865, 2345, 3215, 14345, 24324};
+
+            Console.WriteLine(Iterative(needle, haystack));
+            Console.ReadLine();
+        }
+
+        static public int Iterative(int needle, int[] haystack)
+        {
+            int minValue = 0;
+            int maxValue = haystack.Length;
+
+            while (minValue <= maxValue)
+            {
+                var mid = (int)Math.Round((maxValue + minValue) / 2d, 0);
+
+                if (haystack[mid] < needle)
+                {
+                    minValue = mid + 1;
+                }
+                else if (haystack[mid] > needle)
+                {
+                    maxValue = mid - 1;
+                }
+                else
+                {
+                    return mid;
+                }
+            }
+            return -1;
+        }
+    }
+}
---
+++ RandomRemoval/RandomRemoval.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+// Removing a Number from Array 
+/*     
+Begin with an array of n integers in no particular order.  Remove one of these.  Which integer was removed?
+*/
+
+/*Instead of sorting, it seemed more efficient for me to get an initial count, 
+and then a count once an integer was removed.  The difference between the sums
+leaves the removed integer.  This executed faster as n got large.
+*/
+
+namespace Kata
+{
+    class RandomRemoval
+    {
+        static void Main() //Main executing function
+        {
+            Console.WriteLine("Enter the number of total elements in the array:");
+            string elements = Console.ReadLine(); //Gather input
+            int arrayLength;
+            if (int.TryParse(elements, out arrayLength))
+            {
+                int[] array = InitArray(arrayLength);
+                int initSum = SumArray(array);
+                int finalSum = SumArray(RemoveOneElement(array));
+
+                int removedInt = initSum - finalSum;
+
+                Console.WriteLine("The removed integer was: " + removedInt.ToString());
+                Console.WriteLine("Press any key to exit.");
+                Console.ReadKey();
+            }
+            else //Error checking incase input is not an int
+            {
+                Main(); 
+            }
+        }
+
+        public static int[] InitArray(int arrayLength) //Initialize an array with n elements
+        {
+            int[] array = new int[arrayLength];
+            for (int i = 0; i < arrayLength; i++)
+            {
+                array[i] = i;
+            }
+            return array;
+        }
+
+        public static int SumArray(int[] array) //Reusable, generic method to sum array with n elements
+        {
+            int total = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                total += array[i];
+            }
+            return total;
+        }
+
+        public static int[] RemoveOneElement(int[] array) //Convert array to a List, randomly remove an element, return as array
+        {
+            Random rnd = new Random();
+            List<int> tempList = array.OfType<int>().ToList();
+            int r = rnd.Next(tempList.Count);
+            tempList.RemoveAt(r);
+
+            return tempList.ToArray();
+        }
+    }
+
+}
