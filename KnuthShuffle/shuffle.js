f7b13a13b778353991a98042e8c05664185a080f
Neil Allen <neil.denis.allen@gmail.com>
2013-08-18 11:17:10 -0400

    New kata; js implementation

---
+++ KnuthShuffle/shuffle.js
@@ -0,0 +1,10 @@
+var knuthShuffle = function(array) {
+  var m = array.length, t, i;
+  while (m) {
+    i = Math.floor(Math.random() * m--);
+    t = array[m];
+    array[m] = array[i];
+    array[i] = t;
+  }
+  return array;
+}
